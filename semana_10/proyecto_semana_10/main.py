c4f384fd3c2c97b79e3b53443ee2eb61466c79d3
Diego Hidalgo <luisdiegohidalgo@gmail.com>
2025-10-05 17:59:29 -0600

    Agregué últimos catchers de errores y actualicé archivo 'Semana 5/Ejercicios de Iterables & Listas/iterables_2A.py'.

--- semana_10/proyecto_semana_10/main.py
+++ semana_10/proyecto_semana_10/main.py
@@ -1,7 +1,7 @@
 
 # # # # # Start of MENU # # # # #
 
-def show_menu(): # Write error-catchers
+def show_menu():
     menu_choice = input("""
 Menu options:
 
@@ -13,9 +13,15 @@ Menu options:
     6. View the top 3 students as per their grade average.
     7. View the grade average among all the students.
 
-Please enter the number of the action you wish to take: """)
+Please enter a number (1-7) for the action you wish to take: """)
     print("\n")
-    return menu_choice
+    
+    while True:
+        if menu_choice.isdigit() and 1 <= int(menu_choice) <= 7:
+            return menu_choice
+        else:
+            menu_choice = input("Invalid choice. Please enter a number from 1 to 7: ")
+            print("\n")
 
 
 def route_menu_choice(menu_choice, students_list):
@@ -110,18 +116,24 @@ def input_student_name(students_list):
             return student_name
 
 
-def input_student_section(): # Write error-catchers
-    student_section = input("Please enter the student's section: ")
-    return student_section
-
-
-def validate_grade_number(prompt):
+def input_student_section():
+    pattern = r"^(1[0-2]|[1-9])[A-Ca-c]$"
     while True:
-        number = input(prompt)
+        student_section = input("Please enter the student's section: ").upper()
+
+        if re.match(pattern, student_section):
+            return student_section
+        else:
+            print("\nInvalid section format. Please try again. ")
+
+
+def validate_grade(prompt):
+    while True:
+        grade = input(prompt)
         try:
-            valid_number = float(number)
-            if 1 <= valid_number <= 100:
-                return valid_number
+            valid_grade = float(grade)
+            if 1 <= valid_grade <= 100:
+                return valid_grade
             else:
                 print("\nInvalid answer. Please enter a grade from 1 to 100.")
         except ValueError:
@@ -129,10 +141,10 @@ def validate_grade_number(prompt):
 
 
 def input_grades():
-    spanish_grade = validate_grade_number("Please enter the student's Spanish grade: ")
-    english_grade = validate_grade_number("Please enter the student's English grade: ")
-    social_grade = validate_grade_number("Please enter the student's Social Studies grade: ")
-    science_grade = validate_grade_number("Please enter the student's Science grade: ")
+    spanish_grade = validate_grade("Please enter the student's Spanish grade: ")
+    english_grade = validate_grade("Please enter the student's English grade: ")
+    social_grade = validate_grade("Please enter the student's Social Studies grade: ")
+    science_grade = validate_grade("Please enter the student's Science grade: ")
 
     return spanish_grade, english_grade, social_grade, science_grade
 
@@ -177,7 +189,7 @@ def ask_if_another_student():
 def modify_students_list(students_list):
     while True:
         student_name = student_name = input_student_name(students_list)
-        student_section = input_student_section() # Agregar error-catchers
+        student_section = input_student_section()
         spanish_grade, english_grade, social_grade, science_grade = input_grades()
         individual_avg_rounded = calculate_individual_avg(spanish_grade, english_grade, social_grade, science_grade)
         dict_entry = create_dict_entry(student_name, student_section, spanish_grade, english_grade, social_grade, science_grade, individual_avg_rounded)
--- semana_10/proyecto_semana_10/pruebas.py
+++ semana_10/proyecto_semana_10/pruebas.py
@@ -1,24 +1,16 @@
 
+import re
 
-def input_grades():
-    spanish_grade = float(input("Please enter the student's Spanish grade: "))
-    english_grade = float(input("Please enter the student's English grade: "))
-    social_grade = float(input("Please enter the student's Social Studies grade: "))
-    science_grade = float(input("Please enter the student's Science grade: "))
+def input_student_section():
+    pattern = r"^(1[0-2]|[1-9])[A-Ca-c]$"
+    while True:
+        student_section = input("Please enter the student's section: ").upper()
 
-    return spanish_grade, english_grade, social_grade, science_grade
-
-
-def calculate_individual_avg(spanish_grade, english_grade, social_grade, science_grade): # Optimize to not be limited to just 4 subjects
-    individual_avg = (spanish_grade + english_grade + social_grade + science_grade) / 4
-    individual_avg_rounded = round(individual_avg, 2)
-
-    print(individual_avg_rounded)
+        if re.match(pattern, student_section):
+            print(student_section)
+            break
+        else:
+            print("\nInvalid section format. Please try again. ")
 
 
-def main():
-    spanish_grade, english_grade, social_grade, science_grade = input_grades()
-    calculate_individual_avg(spanish_grade, english_grade, social_grade, science_grade)
-
-
-main()
+input_student_section()
