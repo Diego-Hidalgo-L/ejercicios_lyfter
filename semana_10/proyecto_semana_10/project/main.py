4e86820386f9c3b757b60f3d6b2d4c42b756deff
Diego Hidalgo <luisdiegohidalgo@gmail.com>
2025-10-05 19:01:53 -0600

    Fix de errores entre los módulos.

--- semana_10/proyecto_semana_10/project/main.py
+++ semana_10/proyecto_semana_10/project/main.py
@@ -4,10 +4,11 @@ from menu import show_menu, route_menu_choice
 
 
 def main():
-    students_list = read_csv_file_and_extract_students_list("semana_10/proyecto_semana_10/students_info.csv")
+    students_list = read_csv_file_and_extract_students_list("semana_10/proyecto_semana_10/project/students_info.csv")
     menu_choice = show_menu()
     route_menu_choice(menu_choice, students_list)
-    write_csv_file("semana_10/proyecto_semana_10/students_info.csv", students_list, students_list[0].keys())
+    if students_list:
+        write_csv_file("semana_10/proyecto_semana_10/project/students_info.csv", students_list, students_list[0].keys())
 
 
 main()
