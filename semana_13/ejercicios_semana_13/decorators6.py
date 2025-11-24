8eba7bc177f413486bc283e3b66bf1acb5123ac8
Diego Hidalgo <luisdiegohidalgo@gmail.com>
2025-11-24 10:25:13 -0600

    Modificaciones pequeñas al ejercicio decorators6.py

--- semana_13/ejercicios_semana_13/decorators6.py
+++ semana_13/ejercicios_semana_13/decorators6.py
@@ -1,6 +1,15 @@
 
 from datetime import date
 
+def log_call(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, *kwargs.values())
+        print(f"func: {func.__name__} - args: {args} - {date.today()} - Resultado: {result}")
+        return result
+
+    return wrapper
+
+
 def validate_numbers(func):
     def wrapper(*args, **kwargs):
         all_params = [*args, *kwargs.values()]
@@ -16,30 +25,19 @@ def validate_numbers(func):
                 print(f"Error:'{param}' is not an integer.")
                 continue
         
-        print(valid_params)
         return func(*valid_params)
     
     return wrapper
 
 
-def log_call(func):
-    def wrapper(*args, **kwargs):
-        result = func(*args, *kwargs.values())
-        print(f"func: {func.__name__} - args: {args} - {date.today()} - Resultado: {result}")
-        return result
-
-    return wrapper
-
-
-@validate_numbers
 @log_call
+@validate_numbers
 def multiply(*args):
     multiplier = 1
 
     for arg in args:
         multiplier *= arg
     
-    print(multiplier)
     return multiplier
 
 
