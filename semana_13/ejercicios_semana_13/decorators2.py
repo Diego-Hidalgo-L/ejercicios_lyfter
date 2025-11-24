a12953ac297afc5570ce8e8662d81da02bffea8c
Diego Hidalgo <luisdiegohidalgo@gmail.com>
2025-11-24 15:20:20 -0600

    Modificación a decorators2.py

--- semana_13/ejercicios_semana_13/decorators2.py
+++ semana_13/ejercicios_semana_13/decorators2.py
@@ -3,25 +3,28 @@
 def int_checker(func):
     def wrapper(*args, **kwargs):
         all_params = [*args, *kwargs.values()]
+        int_params = []
 
         for param in all_params:
             try:
                 if isinstance(param, int):
-                    # print(f"{arg} is an integer.")
-                    continue
+                    int_params.append(param)
                 else:
                     raise ValueError
             except ValueError:
                 print(f"ValueError: '{param}' is not an integer.")
 
-        return func(*args, **kwargs)
+        return func(int_params)
     
     return wrapper
 
 
 @int_checker
-def print_args(*args, **kwargs):
-    print(args, kwargs)
+def print_args(*args):
+    print("All integer parameters are:")
+
+    for arg in args:
+        print(arg)
 
 
 print_args(1, "hello", 2, "my", 3, "friend", last_param=4)
