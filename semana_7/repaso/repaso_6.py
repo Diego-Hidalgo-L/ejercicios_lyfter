43eb9467ca5b218a7060efe3ecc6b6bfa66062a3
Diego Hidalgo <luisdiegohidalgo@gmail.com>
2026-02-18 08:09:21 -0600

    Continuación repaso semana_7

--- semana_7/repaso/repaso_6.py
+++ semana_7/repaso/repaso_6.py
@@ -1,7 +1,23 @@
 
 def divide():
-    pass
+    while True:
+        try:
+            a = float(input("Ingrese el primer número: "))
+            b = float(input("Ingrese el segundo número: "))
+            if b == 0:
+                raise ZeroDivisionError
+            return a / b
+        except TypeError:
+            print("Ingrese valores válidos.")
+        except ValueError:
+            print("Ingrese valores válidos.")
+        except ZeroDivisionError:
+            print("No se puede dividir entre 0.")
 
 
 def main():
-    pass
+    result = divide()
+    print("El resultado de la división es:", result)
+
+
+main()
