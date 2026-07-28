92ae29714d3da30d218118da2f0b40a17e6a3354
Diego Hidalgo <luisdiegohidalgo@gmail.com>
2026-07-27 22:25:02 -0600

    Modificación de ejercicios_normalizacion

---
+++ bases_de_datos/normalizacion/ejercicios_normalizacion/ejercicio_1/README.md
@@ -0,0 +1,6 @@
+
+OBSERVACIONES INICIALES:
+
+1) La 1NF está bien aplicada en la tabla inicial, ya que ninguna celda contiene más de un dato. También, no hay columnas que repiten el mismo dato, como podría ser el caso con algunos Customers que tienen más de un Order.
+2) Hay que aplicar correctamente la 2NF y la 3NF, ya que hay información que se repite varias veces a lo largo de la tabla inicial, como Customer Name, Customer Phone, Address y Item Name.
+3) Será necesario separar estos datos en tablas diferentes que incluyan únicamente los datos que dependen de ese PK.
--- bases_de_datos/normalizacion/ejercicios_normalizacion/ejercicio_2/-- schema_2.sql
+++ bases_de_datos/normalizacion/ejercicios_normalizacion/ejercicio_2/-- schema_2.sql
@@ -1,15 +1,27 @@
 -- SQLite
 
+
+-- OWNERS:
+-- Esta tabla incluye sólo la información que depende directamente del Customer y de nada más.
+
 CREATE TABLE owners (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(10) NOT NULL,
     phone_number CHAR(12) NOT NULL
 );
 
+
+-- MAKES:
+-- Esta tabla incluye sólo la información que depende directamente del Make y de nada más.
+
 CREATE TABLE makes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     make VARCHAR(10) NOT NULL
 );
+
+
+-- MODELS:
+-- 1:N entre Make:Models - Cada Make puede tener varios Models, pero cada Model puede pertenecer únicamente a un solo Make.
 
 CREATE TABLE models (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,11 +31,19 @@ CREATE TABLE models (
     color VARCHAR(10) NOT NULL
 );
 
+
+-- CARS:
+-- 1:N entre Models:VIN - Cada Model puede estar en varios VIN, pero cada VIN puede tener únicamente un solo Model.
+
 CREATE TABLE cars (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     vin VARCHAR(11) NOT NULL,
     model_id INTEGER REFERENCES models(id)
 );
+
+
+-- CAR OWNERS:
+-- Insurance company e Insurance policy dependen de la llave compuesta Car ID / Owner ID, por se mantienen dentro de la misma tabla.
 
 CREATE TABLE car_owners (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
