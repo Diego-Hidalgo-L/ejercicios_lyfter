5b9f75964f126f0b802355b6a551e54245bdddad
Diego Hidalgo <luisdiegohidalgo@gmail.com>
2026-08-04 18:54:53 -0600

    Renombramiento de archivos en bases_de_datos/normalizacion

--- bases_de_datos/normalizacion/ejercicios_extra_normalizacion/ejercicio_5/schema_5.sql
+++ bases_de_datos/normalizacion/ejercicios_extra_normalizacion/ejercicio_5/schema_5.sql
@@ -1,16 +1,16 @@
 -- -- SQLite
 
--- CREATE TABLE patients (
---     id INTEGER PRIMARY KEY AUTOINCREMENT,
---     patient_name VARCHAR(20) NOT NULL,
---     patient_phone CHAR(9)
--- );
+CREATE TABLE patients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    patient_name VARCHAR(20) NOT NULL,
+    patient_phone CHAR(9)
+);
 
--- CREATE TABLE doctors (
---     id INTEGER PRIMARY KEY AUTOINCREMENT,
---     doctor_name VARCHAR(15) NOT NULL,
---     specialty VARCHAR(15) NOT NULL
--- );
+CREATE TABLE doctors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    doctor_name VARCHAR(15) NOT NULL,
+    specialty VARCHAR(15) NOT NULL
+);
 
 CREATE TABLE appointments (
     id CHAR(3) PRIMARY KEY NOT NULL,
---
+++ bases_de_datos/normalizacion/ejercicios_normalizacion/ejercicio_1/queries_1.sql
@@ -0,0 +1,17 @@
+-- SQLite
+
+SELECT *
+    FROM customers;
+
+SELECT *
+    FROM addresses;
+
+SELECT *
+    FROM items;
+
+SELECT *
+    FROM orders;
+
+SELECT *
+    FROM order_items;
+
