01f84c677c9342b7bb70b82df273e1abfb8522e1
Diego Hidalgo <luisdiegohidalgo@gmail.com>
2026-08-21 18:52:23 -0600

    Finalización de ejercicios regulares de la tarea 'SQL desde Python'

--- flask/python_sql/ejercicio_python_sql/tarea_3/app.py
+++ flask/python_sql/ejercicio_python_sql/tarea_3/app.py
@@ -1,193 +1,62 @@
-from flask import Flask, request, jsonify
-from db import db_manager
-from py_functions import format_users, format_cars, format_rentals
+from flask import Flask, request
+from repositories import users_repo, cars_repo, rentals_repo
 
 app = Flask(__name__)
 
 # USERS:
 @app.route("/users")
 def get_users():
-    try:
-        filters = request.args
-        valid_columns = ["id", "full_name", "email", "username", "password", "date_of_birth", "status"]
-        filter_columns = []
-        filter_values = []
-
-        if filters:
-
-            for column, value in filters.items():
-                column = column.strip().lower()
-
-                if column in valid_columns:
-                    filter_columns.append(f"{column} = %s")
-                    filter_values.append(value)
-                else:
-                    return jsonify(error_message=f"Invalid filter: {column}"), 400
-
-            where_clause = " AND ".join(filter_columns)
-            query = f"SELECT * FROM users WHERE {where_clause};" 
-
-        else:
-            query = "SELECT * FROM users;"
-
-        results = db_manager.fetchall(query, *filter_values)
-        return [format_users(user) for user in results]
-
-    except Exception as error:
-        return jsonify(error_message=f"Error getting users {error}"), 500
+    filters = request.args
+    return users_repo.get_users(filters)
 
 
 @app.route("/users", methods=["POST"])
 def add_user():
-    try:
-        request_body = request.json
-        values = (
-            request_body.get("full_name"),
-            request_body.get("email"),
-            request_body.get("username"),
-            request_body.get("password"),
-            request_body.get("date_of_birth"),
-            request_body.get("status")
-        )
-
-        # No valido si el email o el username ya existen, porque tiene el constraint de UNIQUE en la DB.
-
-        query = """
-            INSERT INTO users (full_name, email, username, password, date_of_birth, status)
-            VALUES (%s, %s, %s, %s, %s, %s);
-            """
-
-        execute_result = db_manager.execute_query(query, *values)
-
-        if not execute_result:
-            raise Exception("Error executing query")
-        
-        db_manager.commit()
-        return jsonify(message="User added successfully!"), 201
-
-    except Exception as error:
-        db_manager.rollback()
-        return jsonify(error_message=f"Error adding user to database: {error}"), 400
+    request_body = request.json
+    return users_repo.add_user(request_body)
 
 
 @app.route("/users/<identifier>", methods=["PATCH"])
+def update_user_status(identifier):
+    request_body = request.json
+    return users_repo.update_user_status(identifier, request_body)
 
 
 # CARS:
 @app.route("/cars")
 def get_cars():
     filters = request.args
-    valid_columns = ["id", "license_plate", "brand", "model", "year", "status"]
-    filter_columns = []
-    filter_values = []
-
-    if filters:
-
-        for column, value in filters.items():
-            column = column.strip().lower()
-
-            if column in valid_columns:
-                filter_columns.append(f"{column} = %s")
-                filter_values.append(value)
-            else:
-                return jsonify(error_message=f"Invalid filter: {column}"), 400
-
-        where_clause = " AND ".join(filter_columns)
-        query = f"SELECT * FROM cars WHERE {where_clause};" 
-
-    else:
-        query = "SELECT * FROM cars;"
-
-    results = db_manager.fetchall(query, *filter_values)
-    return [format_cars(car) for car in results]
+    return cars_repo.get_cars(filters)
 
 
 @app.route("/cars", methods=["POST"])
 def add_car():
-    try:
-        request_body = request.json
-        values = (
-            request_body.get("license_plate"),
-            request_body.get("brand"),
-            request_body.get("model"),
-            request_body.get("year"),
-            request_body.get("status")
-        )
-
-        query = """
-            INSERT INTO cars (license_plate, brand, model, year, status)
-            VALUES (%s, %s, %s, %s, %s);
-            """
-
-        execute_result = db_manager.execute_query(query, *values)
-
-        if not execute_result:
-            raise Exception("Error executing query")
-        
-        db_manager.commit()
-        return jsonify(message="Car added successfully"), 201
-
-    except Exception as error:
-        db_manager.rollback()
-        return jsonify(error_message=f"Error adding car to database: {error}"), 400
+    request_body = request.json
+    return cars_repo.add_car(request_body)
 
 
 @app.route("/cars/<identifier>", methods=["PATCH"])
-
+def update_car_status(identifier):
+    request_body = request.json
+    return cars_repo.update_car_status(identifier, request_body)
 
 # RENTALS:
 @app.route("/rentals")
 def get_rentals():
     filters = request.args
-    valid_columns = ["id", "car_id", "user_id", "rental_date", "status", "return_date"]
-    filter_columns = []
-    filter_values = []
-
-    if filters:
-
-        for column, value in filters.items():
-            column = column.strip().lower()
-
-            if column in valid_columns:
-                filter_columns.append(f"{column} = %s")
-                filter_values.append(value)
-            else:
-                return jsonify(error_message=f"Invalid filter: {column}"), 400
-
-        where_clause = " AND ".join(filter_columns)
-        query = f"SELECT * FROM rentals WHERE {where_clause};" 
-
-    else:
-        query = "SELECT * FROM rentals;"
-
-    results = db_manager.fetchall(query, *filter_values)
-    return [format_rentals(rental) for rental in results]
+    return rentals_repo.get_rentals(filters)
 
 
 @app.route("/rentals", methods=["POST"])
 def add_rental():
-    try:
-        request_body = request.json
-        user_id = request_body.get("user_id")
-        car_id = request_body.get("car_id")
-        rental_date = request_body.get("rental_date")
-        columns = (user_id, car_id, rental_date)
-
-        query = "SELECT add_rental(%s, %s, %s)"
-
-        db_manager.execute_query(query, *columns)
-        db_manager.commit()
-
-        return jsonify(message="Rental added successfully"), 201
-
-    except Exception as error:
-        db_manager.rollback()
-        return jsonify(error_message=f"Error adding rental: {error}"), 400
+    request_body = request.json
+    return rentals_repo.add_rental(request_body)
 
 
-@app.route("/rentals/<identifier>", methods=["PATCH"])
-def patch_rentals():
-    pass
+@app.route("/rentals/<car_id>", methods=["PATCH"])
+def update_rental_status(car_id):
+    request_body = request.json
+    return rentals_repo.update_rental_status(car_id, request_body)
 
 
 if __name__ == "__main__":
--- flask/python_sql/ejercicio_python_sql/tarea_3/repositories.py
+++ flask/python_sql/ejercicio_python_sql/tarea_3/repositories.py
@@ -1,96 +1,273 @@
+from flask import jsonify
+from db import db_manager
+from functions import format_users, format_cars, format_rentals
+from datetime import date
 
-class UsersRepository:
+class UsersRepo:
     def __init__(self, db_manager):
         self.db_manager = db_manager
 
-    def add_user(self, full_name, email, username, password, date_of_birth, status): # Debería crear validaciones para cada parámetro?
+    def get_users(self, filters):
         try:
-            self.db_manager.execute_query("""
-                INSERT INTO lyfter_car_rental.users
-                VALUES (%s, %s, %s, %s, %s, %s);""",
-                (full_name, email, username, password, date_of_birth, status)
-                )
+            valid_columns = ["id", "full_name", "email", "username", "password", "date_of_birth", "status"]
+            filter_columns = []
+            filter_values = []
 
-            print("User entered successfully")
-            return True
+            if filters:
+                for column, value in filters.items():
+                    column = column.strip().lower()
+
+                    if column in valid_columns:
+                        filter_columns.append(f"{column} = %s")
+                        filter_values.append(value)
+                    else:
+                        return jsonify(error_message=f"Invalid filter: {column}"), 400
+
+                where_clause = " AND ".join(filter_columns)
+                query = f"SELECT * FROM users WHERE {where_clause};" 
+
+            else:
+                query = "SELECT * FROM users;"
+
+            results = self.db_manager.fetchall(query, *filter_values)
+            return [format_users(user) for user in results]
 
         except Exception as error:
-            print("Error entering new user:", error)
-            return False
+            return jsonify(error_message=f"Error getting users {error}"), 500
 
-    def update_status(self, _id, status): # Debería crear una validación para estos parameters también?
+    def add_user(self, request_body):
         try:
-            self.db_manager.execute_query("""
-                UPDATE lyfter_car_rental.users
-                SET status = %s
-                WHERE id = %s""",
-                (_id, status)
-                )
-
-            print("User status updated successfully")
-            return True
-
-        except Exception as error:
-            print("Error updating user status:", error)
-            return False
-
-
-class CarsRepository:
-    def __init__(self, db_manager):
-        self.db_manager = db_manager
-
-    def add_car(self, brand, model, year, status):
-        try:
-            self.db_manager.execute_query("""
-                INSERT INTO lyfter_car_rental.cars
-                VALUES (%s, %s, %s, %s);""",
-                (brand, model, year, status)
-                )
-
-            print("Car entered successfully")
-            return True
-        
-        except Exception as error:
-            print("Error entering new car:", error)
-            return False
-
-    def update_status(self, status, _id): # Debería crear una validación para estos parameters también?
-        try:
-            self.db_manager.execute_query("""
-                UPDATE lyfter_car_rental.cars
-                SET status = %s
-                WHERE id = %s;""",
-                (status, _id)
-                )
-
-            print("User status updated successfully")
-            return True
-
-        except Exception as error:
-            print("Error updating user status:", error)
-            return False
-
-
-class RentalsRepository:
-    def __init__(self, db_manager):
-        self.db_manager = db_manager
-
-    def add_rental(self, car_id, user_id, rental_date, status):
-        try:
-            self.db_manager.execute_query("""
-                INSERT INTO rentals
-                VALUES (%s, %s, %s, %s);""",
-                (car_id, user_id, rental_date, status)
-                )
-
-            print("Rental entered successfully")
-            return True
-
-        except Exception as error:
-            print("Error entering new rental:", error)
-            return False
-
-    def return_rental(self):
-        self.db_manager.execute_query("""
-            """
+            values = (
+                request_body.get("full_name"),
+                request_body.get("email"),
+                request_body.get("username"),
+                request_body.get("password"),
+                request_body.get("date_of_birth"),
+                request_body.get("status")
             )
+
+            # No valido si el email o el username ya existen, porque tiene el constraint de UNIQUE en la DB.
+
+            query = """
+                INSERT INTO users (full_name, email, username, password, date_of_birth, status)
+                VALUES (%s, %s, %s, %s, %s, %s);
+                """
+
+            execute_result = self.db_manager.execute_query(query, *values)
+
+            if not execute_result:
+                raise Exception("Error executing query")
+            
+            self.db_manager.commit()
+
+            return jsonify(message="User added successfully!"), 201
+
+        except Exception as error:
+            self.db_manager.rollback()
+            return jsonify(error_message=f"Error adding user to database: {error}"), 400
+
+    def update_user_status(self, identifier, request_body):
+        try:
+            status = request_body.get("status")
+            # Si no viene key "status", el try/except lo atrapará.
+
+            query = """
+                UPDATE users
+                SET status = %s
+                WHERE id = %s;
+                """
+
+            execute_result = self.db_manager.execute_query(query, status, identifier)
+
+            if not execute_result:
+                raise Exception("Error executing query")
+            
+            self.db_manager.commit()
+            return jsonify(message=f"User {identifier} status updated successfully!")
+
+        except Exception as error:
+            self.db_manager.rollback()
+            return jsonify(error_message=f"Error updating user status: {error}"), 400
+
+
+class CarsRepo:
+    def __init__(self, db_manager):
+        self.db_manager = db_manager
+
+    def get_cars(self, filters):
+        valid_columns = ["id", "license_plate", "brand", "model", "year", "status"]
+        filter_columns = []
+        filter_values = []
+
+        if filters:
+            for column, value in filters.items():
+                column = column.strip().lower()
+
+                if column in valid_columns:
+                    filter_columns.append(f"{column} = %s")
+                    filter_values.append(value)
+                else:
+                    return jsonify(error_message=f"Invalid filter: {column}"), 400
+
+            where_clause = " AND ".join(filter_columns)
+            query = f"SELECT * FROM cars WHERE {where_clause};" 
+
+        else:
+            query = "SELECT * FROM cars;"
+
+        results = self.db_manager.fetchall(query, *filter_values)
+        return [format_cars(car) for car in results]
+
+    def add_car(self, request_body):
+        try:
+            values = (
+                request_body.get("license_plate"),
+                request_body.get("brand"),
+                request_body.get("model"),
+                request_body.get("year"),
+                request_body.get("status")
+            )
+
+            query = """
+                INSERT INTO cars (license_plate, brand, model, year, status)
+                VALUES (%s, %s, %s, %s, %s);
+                """
+
+            execute_result = self.db_manager.execute_query(query, *values)
+
+            if not execute_result:
+                raise Exception("Error executing query")
+            
+            self.db_manager.commit()
+            return jsonify(message="Car added successfully"), 201
+
+        except Exception as error:
+            self.db_manager.rollback()
+            return jsonify(error_message=f"Error adding car to database: {error}"), 400
+
+    def update_car_status(self, identifier, request_body):
+        try:
+            status = request_body.get("status")
+
+            query = """
+                UPDATE cars
+                SET status = %s
+                WHERE id = %s;
+                """
+
+            execute_result = self.db_manager.execute_query(query, status, identifier)
+
+            if not execute_result:
+                raise Exception("Error executing query")
+            
+            self.db_manager.commit()
+            return jsonify(message=f"Car {identifier} status updated successfully!"), 200
+
+        except Exception as error:
+            self.db_manager.rollback()
+            return jsonify(error_message=f"Error updating car status: {error}"), 400
+
+
+class RentalsRepo:
+    def __init__(self, db_manager):
+        self.db_manager = db_manager
+
+    def get_rentals(self, filters):
+        valid_columns = ["id", "car_id", "user_id", "rental_date", "status", "return_date"]
+        filter_columns = []
+        filter_values = []
+
+        if filters:
+            for column, value in filters.items():
+                column = column.strip().lower()
+
+                if column in valid_columns:
+                    filter_columns.append(f"{column} = %s")
+                    filter_values.append(value)
+                else:
+                    return jsonify(error_message=f"Invalid filter: {column}"), 400
+
+            where_clause = " AND ".join(filter_columns)
+            query = f"SELECT * FROM rentals WHERE {where_clause};" 
+
+        else:
+            query = "SELECT * FROM rentals;"
+
+        results = self.db_manager.fetchall(query, *filter_values)
+        return [format_rentals(rental) for rental in results]
+
+    def add_rental(self, request_body):
+        try:
+            user_id = request_body.get("user_id")
+            car_id = request_body.get("car_id")
+            rental_date = request_body.get("rental_date")
+            columns = (user_id, car_id, rental_date)
+
+            query = "SELECT add_rental(%s, %s, %s)"
+
+            execute_result = self.db_manager.execute_query(query, *columns)
+
+            if not execute_result:
+                raise Exception("Error executing query")
+
+            self.db_manager.commit()
+            return jsonify(message="Rental added successfully"), 201
+
+        except Exception as error:
+            self.db_manager.rollback()
+            return jsonify(error_message=f"Error adding rental: {error}"), 400
+
+    def update_rental_status(self, car_id, request_body):
+        try:
+            status = request_body.get("status")
+            return_date = request_body.get("return_date")
+
+            if status == "returned" and not return_date:
+                return_date = date.today()
+
+            results = self.db_manager.fetchall("SELECT * FROM rentals WHERE car_id = %s AND status = 'ongoing';", car_id)
+
+            # Este diseño no permite cambiar el status de 'returned' a 'ongoing', lo cual tiene cierto sentido por seguridad.
+            # Pero dejo los comentarios abajo por si fuera necesario modificar esa lógica.
+
+            if not results:
+                return jsonify(error_message=f"There are no ongoing rentals for car ID {car_id}"), 400
+
+            # if status == "returned":
+            query = """
+                UPDATE rentals
+                SET status = %s, return_date = %s
+                WHERE car_id = %s;
+
+                UPDATE cars
+                SET status = 'available'
+                WHERE id = %s;
+                """
+            # elif status == "ongoing":
+            #     query = """
+            #         UPDATE rentals
+            #         SET status = %s, return_date = null
+            #         WHERE car_id = %s;
+
+            #         UPDATE cars
+            #         SET status = 'rental ongoing'
+            #         WHERE id = %s;
+            #         """
+
+            execute_result = self.db_manager.execute_query(query, status, return_date, car_id, car_id)
+
+            if not execute_result:
+                raise Exception("Error executing query")
+
+            self.db_manager.commit()
+            return jsonify(message=f"Rental status for car ID {car_id} updated successfully!"), 200
+
+        except Exception as error:
+            self.db_manager.rollback()
+            return jsonify(error_message=f"Error updating rental status: {error}"), 400
+
+
+# CLASS INSTANCES:
+users_repo = UsersRepo(db_manager)
+cars_repo = CarsRepo(db_manager)
+rentals_repo = RentalsRepo(db_manager)
