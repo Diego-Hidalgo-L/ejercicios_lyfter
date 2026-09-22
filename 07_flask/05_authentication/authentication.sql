8bfcab555d7e5063b061f27cb61726f43c65f3d2
Diego Hidalgo <luisdiegohidalgo@gmail.com>
2026-09-22 11:27:24 -0600

    Actualización de archivo authentication.sql

--- 07_flask/05_authentication/authentication.sql
+++ 07_flask/05_authentication/authentication.sql
@@ -5,14 +5,17 @@ DROP TABLE invoices;
 DROP TABLE products;
 DROP TABLE users;
 
-SELECT * FROM authentication.invoices
+SELECT * FROM authentication.users
 ORDER BY id ASC;
 
 DELETE FROM authentication.invoice_products;
 DELETE FROM authentication.invoices;
 
-ALTER TABLE authentication.invoices
-ALTER COLUMN id RESTART WITH 1;
+DELETE FROM authentication.users
+WHERE id = 9;
+
+ALTER TABLE authentication.users
+ALTER COLUMN id RESTART WITH 7;
 
 ALTER TABLE authentication.invoice_products
 ALTER COLUMN id RESTART WITH 1;
